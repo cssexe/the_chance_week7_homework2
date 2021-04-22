a35c2fee43044056621608278af306adf503528a
Mohammed Nasser <ab07daa@gmail.com>
2021-04-22 03:09:12 +0300

    add Shadow to textView

--- app/src/main/java/com/ibareq/the_chance_week7_homework2/MainActivity.kt
+++ app/src/main/java/com/ibareq/the_chance_week7_homework2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ibareq.the_chance_week7_homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
